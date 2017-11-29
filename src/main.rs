4da5c0a54c9bb435374350519861dedcec277cd2
jjgccg <joseph.george@richmond.edu>
2017-11-28 21:24:27 -0500

    removed plotting, added mechanism to save results to file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 mod snapping_mech; // contains public snapping mechanism
 
-extern crate dataplotlib;
 extern crate rand;
 extern crate rug;
 
@@ -8,44 +7,21 @@ use rand::{random, Open01};
 use rand::distributions::{IndependentSample, Range};
 use std::f64;
 use std::io;
-use std::iter::repeat;
-use dataplotlib::util::{zip2};
-use dataplotlib::plotbuilder::PlotBuilder2D;
-use dataplotlib::plotter::Plotter;
+use rug::ops::Pow;
 
 // Implementation of laplacian noise generator which uses snapping mechanism in its place
 fn main()
 {
-	// call the snapping mechanism using specified parameter.
-
+	// Snapping Mechanism call: f(D), Lambda, B
 	let dp_noisy_output = snapping_mech::snapping_mechanism(12.0, 2.0, 10000.0);
 	println!("Result of adding noise to query result f(D): ");
     println!("{}", dp_noisy_output);
 
-    // Run the noisy output to generate a plot of distribution of outputs
-    // Will generate a different output each time because of sample from uniform dist
-    let mut output_vals: Vec<f64> = Vec::new();
-    let mut iterations: Vec<f64> = Vec::new();
-    let mut iter = 0.0;
-    for x in 0..1000000
-    {
-    	output_vals.push(snapping_mech::snapping_mechanism(12.0, 2.0, 10000.0));
-    	iterations.push(iter);
-    	iter = iter + 1.0;
-    }
-    // Pass results to plotting function
-    // TODO: need to plot value vs. probability of that value
-    plot_output_dist(&iterations, &output_vals);
-}
+    // Snapping Mechanism call: f(D), Lambda.  B IS FIXED.
+    let dp_noisy_output_2 = snapping_mech::snapping_mechanism_2(12.0, 2.0);
+    println!("Result of adding noise to query result f(D): ");
+    println!("{}", dp_noisy_output_2);
 
-// Plot output values of snapping mechanism with parameters held constant
-fn plot_output_dist(output_vals: & Vec<f64>, iterations: & Vec<f64>)
-{
-	let plot_vals = zip2(iterations, output_vals);
-	let mut pb = PlotBuilder2D::new();
-	pb.add_color_xy(plot_vals, [1.0, 0.0, 0.0, 1.0]);
-
-	let mut plt = Plotter::new();
-    plt.plot2d(pb);
-    plt.join();
+    // Run snapping mechanism 'x' number of times to store results in file for plotting
+    snapping_mech::store_results(12.0, 2.0, 10000.0, 1000000);
 }
--- src/snapping_mech.rs
+++ src/snapping_mech.rs
@@ -5,8 +5,14 @@ use rand::{random, Open01};
 use rand::distributions::{IndependentSample, Range};
 use std::f64;
 use std::io;
+use rug::Float;
+use rug::float::Round;
+use rug::ops::Pow;
+use std::error::Error;
+use std::fs::File;
+use std::io::Write;
 
-/// Returns a database query answer with noise, such that the result is differentially
+/// Returns a database query answer with added noise, such that the result is differentially
 /// private
 ///
 ///# Arguments
@@ -14,8 +20,15 @@ use std::io;
 ///* 'fD' - result of original database query
 ///* 'lambda' - sensitivity/privacy parameter epsilon
 ///* 'B' - a number such that: lambda < B < (2^{46})(lambda)
-pub fn snapping_mechanism(fD: f64, lambda: f64, B: f64) -> f64
+#[no_mangle]
+pub extern "C" fn snapping_mechanism(fD: f64, lambda: f64, B: f64) -> f64
 {
+	// Check to make sure B is within range
+	if (lambda >= B) | (B >= (lambda * 2_f64.pow(46)))
+	{
+		panic!("Invalid B value: must satisfy λ < B < λ*2^(46)");
+	}
+
 	let mut S = 0.0; // 50% chance S is -1 or +1
 	let between = Range::new(0u8, 1u8); //Two possible choices - 0 or 1
 	let mut rng = rand::thread_rng();
@@ -32,6 +45,66 @@ pub fn snapping_mechanism(fD: f64, lambda: f64, B: f64) -> f64
 	let round = round(inner_result, lambda_sub);
 
 	return clamp(round, B)
+}
+
+/// Returns a database query answer with added noise, such that the result is differentially
+/// private.  Fixed B Version.
+///
+///# Arguments
+///
+///* 'fD' - result of original database query
+///* 'lambda' - sensitivity/privacy parameter epsilon
+pub fn snapping_mechanism_2(fD: f64, lambda: f64) -> f64
+{
+	let mut S = 0.0; // 50% chance S is -1 or +1
+	let between = Range::new(0u8, 1u8); //Two possible choices - 0 or 1
+	let mut rng = rand::thread_rng();
+	let num = between.ind_sample(&mut rng);
+	if num == 1 {S = 1.0};
+	if num == 0 {S = -1.0};
+
+	let B = lambda * 2_f64.pow(45); // DEFAULT: Upper bound of B
+
+	let clamp_fD = clamp(fD, B); 
+	let uni_dist_num = uniform_dist(); // LN(U*)
+	let inner_result = clamp_fD + S * lambda * uni_dist_num;
+
+	//calculate outer clamp by passing in inner result rounded to alpha
+	let lambda_sub = lambda_sub(lambda);
+	let round = round(inner_result, lambda_sub);
+
+	return clamp(round, B)
+}
+
+/// Runs the snapping mechanism a user specified number of times, storing
+/// the results into a text file so that it can be viewed, analyzed, and plotted.
+///
+///# Arguments
+///
+///* 'fD' - result of original database query
+///* 'lambda' - sensitivity/privacy parameter epsilon
+///* 'B' - a number such that: lambda < B < (2^{46})(lambda)
+///* 'num_iteration' - numer of times to perform snapping mechanism call
+pub fn store_results(fD: f64, lambda: f64, B: f64, num_iteration: i64)
+{
+	let mut output_vals: Vec<f64> = Vec::new();
+	for x in 0..num_iteration
+    {
+        let current_val = snapping_mechanism(fD, lambda, B);
+        output_vals.push(current_val);
+    }
+
+    let strings: Vec<String> = output_vals.iter().map(|n| n.to_string()).collect();
+    let mut file = match File::create("/home/osboxes/results.txt")
+    {
+    	Err(e) =>
+    	{
+    		println!("Couldn't open results.txt");
+    		return;
+    	},
+    	Ok(file) => file,
+    };
+    writeln!(file, "{}", strings.join(", "));
 }
 
 /// Returns a floating point number related to input x
@@ -64,6 +137,9 @@ fn uniform_dist() -> f64
 {
 	// Generates floating point numbers uniformly in open interval (0,1)
     let Open01(sample) = random::<Open01<f64>>();
+
+    // log10(sample) with precision 64 is rounded to nearest 64 bit float
+    let mut val_log = Float::with_val_round(64, f64::log10(sample), Round::Nearest);
 
     return f64::log10(sample);
 }
